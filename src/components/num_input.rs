d020790c9463d91f64953b5204f90e9674e56a52
Benjamin Hall <benjaminjahall@gmail.com>
2022-03-19 01:01:00 -0400

    Implement float input

--- src/components/num_input.rs
+++ src/components/num_input.rs
@@ -1,4 +1,5 @@
 use std::fmt::Display;
+use std::marker::PhantomData;
 
 use crossterm::event::KeyCode;
 use num::traits::{FromPrimitive, SaturatingAdd, SaturatingMul, SaturatingSub};
@@ -261,71 +262,183 @@ impl<T: InputUnsignedInt> Component for UnsignedIntInput<T> {
     }
 }
 
+#[derive(Debug)]
 pub struct FloatInput<T: InputFloat> {
-    current: T,
+    value: FloatValue,
+    _phantom: PhantomData<T>,
+}
+
+#[derive(Debug)]
+pub enum FloatValue {
+    Infinity { negative: bool },
+    Nan,
+    Number(FloatNum),
+}
+
+#[derive(Debug)]
+pub struct FloatNum {
+    whole: String,
+    integral: Option<String>,
+    negative: bool,
+}
+
+#[derive(Debug)]
+pub enum NewFloatError {
+    /// The float was neither infinity, Nan, or finite
+    InvalidState,
+    /// The float's string representation isn't a valid decimal
+    ParseError,
+}
+
+fn parse_digit_string(string: &str) -> Result<&str, NewFloatError> {
+    if string.chars().all(|c| char::is_ascii_digit(&c)) {
+        Ok(string)
+    } else {
+        Err(NewFloatError::ParseError)
+    }
 }
 
 impl<T: InputFloat> FloatInput<T> {
-    pub fn new(initial_value: T) -> Self {
-        Self {
-            current: initial_value,
+    pub fn new(initial_value: T) -> Result<Self, NewFloatError> {
+        let value = if initial_value.is_infinite() {
+            FloatValue::Infinity {
+                negative: initial_value.is_sign_negative(),
+            }
+        } else if initial_value.is_nan() {
+            FloatValue::Nan
+        } else if initial_value.is_finite() {
+            let repr = initial_value.to_string();
+            // TODO: make number parsing cleaner
+            let has_decimal = repr.contains('.');
+            let is_negative = repr.chars().next().map_or(false, |c| c == '-');
+            let repr_no_sign = if is_negative { &repr[1..] } else { &repr[..] };
+            if has_decimal {
+                let (first_maybe, second_maybe) = repr_no_sign.split_once('.').unwrap();
+                if first_maybe.is_empty() && second_maybe.is_empty() {
+                    return Err(NewFloatError::ParseError);
+                }
+                FloatValue::Number(FloatNum {
+                    whole: parse_digit_string(first_maybe)?.into(),
+                    integral: Some(parse_digit_string(second_maybe)?.into()),
+                    negative: is_negative,
+                })
+            } else {
+                if repr_no_sign.is_empty() {
+                    return Err(NewFloatError::ParseError);
+                }
+                FloatValue::Number(FloatNum {
+                    whole: parse_digit_string(repr_no_sign)?.into(),
+                    integral: None,
+                    negative: is_negative,
+                })
+            }
+        } else {
+            return Err(NewFloatError::ParseError);
+        };
+        Ok(FloatInput {
+            value,
+            _phantom: PhantomData::default(),
+        })
+    }
+
+    pub fn push_digit(&mut self, digit: char) {
+        if let FloatValue::Number(value) = &mut self.value {
+            if digit.is_ascii_digit() {
+                value
+                    .integral
+                    .as_mut()
+                    .unwrap_or(&mut value.whole)
+                    .push(digit);
+                if value.integral.is_none() {
+                    value.whole = value.whole.trim_start_matches('0').into();
+                }
+            } else if digit == '.' && value.integral.is_none() {
+                value.integral = Some("".into());
+            }
         }
     }
 
-    pub fn set(&mut self, value: T) {
-        self.current = value;
-    }
-
-    pub fn add(&mut self, value: T) -> &mut Self {
-        self.set(self.current + value);
-        self
-    }
-
-    pub fn sub(&mut self, value: T) -> &mut Self {
-        self.set(self.current - value);
-        self
-    }
-
-    pub fn multiply(&mut self, value: T) -> &mut Self {
-        self.set(self.current * value);
-        self
-    }
-
     pub fn remove_digit(&mut self) {
-        // integer division with 10
-        self.set(self.current / T::from_u32(10).unwrap())
+        if let FloatValue::Number(value) = &mut self.value {
+            if let Some(integral) = &mut value.integral {
+                if integral.is_empty() {
+                    value.integral = None;
+                } else {
+                    integral.pop();
+                }
+            } else {
+                value.whole.pop();
+            }
+        }
     }
 
     pub fn value(&self) -> T {
-        self.current
-    }
+        match &self.value {
+            FloatValue::Infinity {
+                negative: is_negative,
+            } => {
+                if *is_negative {
+                    T::neg_infinity()
+                } else {
+                    T::infinity()
+                }
+            }
+            FloatValue::Nan => T::nan(),
+            FloatValue::Number(number) => {
+                let whole_part = if number.whole.is_empty() {
+                    "0"
+                } else {
+                    &number.whole
+                };
+                let entire = if let Some(integral) = &number.integral {
+                    format!("{}.{}", whole_part, integral)
+                } else {
+                    whole_part.to_string()
+                };
 
-    pub fn append_digit(&mut self, digit: char) -> bool {
-        if let Some(dig) = digit.to_digit(10) {
-            // instead of converting to string, just multiply by 10 and add/sub
-            // the digit. This way, we also cap out at the min/max of the number
-            self.multiply(T::from_u32(10).unwrap())
-                .add(T::from_u32(dig).unwrap());
-            true
-        } else {
-            false
+                let raw = T::from_str_radix(&entire, 10)
+                    .map_err(|_| NewFloatError::ParseError)
+                    .unwrap();
+                if number.negative {
+                    -raw
+                } else {
+                    raw
+                }
+            }
         }
     }
 
     pub fn get_span_builder(&self) -> SpanBuilder {
         let mut builder = SpanBuilder::default();
-        builder.push(String::from("> "), Style::default().fg(Color::Green));
-        builder.push(format!("{}", self.current), Style::default());
-        if self.current == T::max_value() {
-            builder.push(
-                String::from(" (max value)"),
-                Style::default().fg(Color::Gray),
-            )
-        } else if self.current == T::min_value() {
-            builder.push(
-                String::from(" (min value)"),
-                Style::default().fg(Color::Gray),
-            )
+        match &self.value {
+            FloatValue::Infinity { negative } => {
+                builder.push(
+                    String::from(if *negative { "- " } else { "+ " }),
+                    Style::default().fg(Color::Green),
+                );
+                builder.push(T::infinity().to_string(), Style::default());
+            }
+            FloatValue::Nan => {
+                builder.push(String::from("> "), Style::default().fg(Color::Green));
+                builder.push(T::nan().to_string(), Style::default());
+            }
+            FloatValue::Number(number) => {
+                let whole_part = if number.whole.is_empty() {
+                    "0"
+                } else {
+                    &number.whole
+                };
+                let entire = if let Some(integral) = &number.integral {
+                    format!("{}.{}", whole_part, integral)
+                } else {
+                    whole_part.to_string()
+                };
+                builder.push(
+                    String::from(if number.negative { "- " } else { "+ " }),
+                    Style::default().fg(Color::Green),
+                );
+                builder.push(entire, Style::default());
+            }
         }
         builder
     }
@@ -339,17 +452,34 @@ impl<T: InputFloat> Component for FloatInput<T> {
         if let Event::Key(key_event) = event {
             match key_event.code {
                 KeyCode::Char(c) => {
-                    self.append_digit(c);
+                    if c.is_ascii_digit() || c == '.' {
+                        self.push_digit(c)
+                    } else if c == '-' {
+                        match &mut self.value {
+                            FloatValue::Number(num) => num.negative = !num.negative,
+                            FloatValue::Infinity {
+                                negative: is_negative,
+                            } => *is_negative = !*is_negative,
+                            _ => {}
+                        }
+                    }
                 }
                 KeyCode::Backspace => {
                     self.remove_digit();
                 }
-                KeyCode::Up => {
-                    self.add(T::one());
-                }
-                KeyCode::Down => {
-                    self.sub(T::one());
-                }
+                KeyCode::Tab => match &self.value {
+                    FloatValue::Number(..) => self.value = FloatValue::Infinity { negative: false },
+                    FloatValue::Infinity { .. } => {
+                        self.value = FloatValue::Nan;
+                    }
+                    FloatValue::Nan => {
+                        self.value = FloatValue::Number(FloatNum {
+                            whole: String::new(),
+                            integral: None,
+                            negative: false,
+                        })
+                    }
+                },
                 KeyCode::Enter => return NumInputResponse::Submit,
                 KeyCode::Esc => return NumInputResponse::Cancel,
                 _ => {}
